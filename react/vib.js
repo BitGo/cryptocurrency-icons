175929327ec5715e22be4cf00ffbc67f804c2d0c
Michael McShinsky <michaelmcshinsky@bitgo.com>
2026-02-20 11:58:14 -0700

    feat: modernize tooling and dependencies
    - Upgrade svgo to v3 with new clean-svgs.js script
    - Upgrade @svgr/core to v8 with explicit plugin architecture
    - Upgrade husky to v9 with new hook format
    - Add validation script with security checks and auto-fix
    - Add sync-check script for React/SVG parity verification
    - Add .nvmrc for Node 20 LTS
    - Update package.json with exports field and engine requirements
    
    Ticket: CE-9916

--- react/vib.js
+++ react/vib.js
@@ -1,3 +1,3 @@
 import * as React from "react";
-const SvgVib = ({ prefixId = "", ...props }) => /* @__PURE__ */ React.createElement("svg", { height: 32, viewBox: "0 0 32 32", width: 32, xmlns: "http://www.w3.org/2000/svg", ...props }, /* @__PURE__ */ React.createElement("g", { fill: "none" }, /* @__PURE__ */ React.createElement("circle", { cx: 16, cy: 16, fill: "#ff1f43", r: 16 }), /* @__PURE__ */ React.createElement("path", { d: "M7 7h4.2l7.2 12.775V7H22v18h-4.8z", fill: "#fff" })));
+const SvgVib = ({ prefixId = "", ...props }) => /* @__PURE__ */ React.createElement("svg", { xmlns: "http://www.w3.org/2000/svg", width: 32, height: 32, viewBox: "0 0 32 32", ...props }, /* @__PURE__ */ React.createElement("g", { fill: "none" }, /* @__PURE__ */ React.createElement("circle", { cx: 16, cy: 16, r: 16, fill: "#ff1f43" }), /* @__PURE__ */ React.createElement("path", { fill: "#fff", d: "M7 7h4.2l7.2 12.775V7H22v18h-4.8z" })));
 export default SvgVib;
